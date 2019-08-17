9aa2b9aab8e200fac595f38609862eb375a7b1ca
Nicholis <nicholis@ndt3.me>
2019-08-17 19:17:04 +0200

    Update pride.js

--- custom/pride.js
+++ custom/pride.js
@@ -7,6 +7,10 @@ window.addEventListener('load', function() {
     pride.addCode("p,r,i,d,e", function() {
         document.getElementsByClassName('page-header')[0].style.backgroundImage='url("https://wd-static1.ndt3.me/img/pride.png")';
         document.getElementsByClassName('page-header')[0].style.backgroundRepeat='round';
+        var h = document.getElementById('header-content');
+        if(h){
+          h.style='background-color: #2b2b2bb5 !important; display: inline-block;';
+        }
       })
       .addHook(function(){
         console.log("Hook called for: " + this.activeEgg.keys);
